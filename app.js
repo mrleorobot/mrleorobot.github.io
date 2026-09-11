54e359c30d9bf336b13219b726e0ac4c711f181a
Leonilson <leosouza5555@gmail.com>
2026-09-11 15:31:27 -0300

    Keep ambient light monochrome and floating controls clear of content

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-import "./awwwards-upgrade.js?v=20260911editorial3";
-import "./script.js?v=20260911editorial3";
-import "./hero-ink.js?v=20260911editorial3";
-import "./evolution.js?v=20260911editorial3";
-import "./visual-polish.js?v=20260911editorial3";
+import "./awwwards-upgrade.js?v=20260911editorial4";
+import "./script.js?v=20260911editorial4";
+import "./hero-ink.js?v=20260911editorial4";
+import "./evolution.js?v=20260911editorial4";
+import "./visual-polish.js?v=20260911editorial4";
